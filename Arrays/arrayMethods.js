6aea1cd99c6713a19523584a73a392969ca15c3a
Methaprian <metha5prian@gmail.com>
2023-02-06 12:01:38 +0530

    Day-6 Commit

---
+++ Arrays/arrayMethods.js
@@ -0,0 +1,31 @@
+let arr=[10,20,30,40,50,45,32]
+let arr1=['a','b','c','d']
+console.log(arr);
+
+function statement(val) {
+    return val>25
+}
+(itr1)=> {
+    return val
+}
+//console.log(arr.concat(arr1));
+// console.log(arr.join('->'));
+// console.log(arr.lastIndexOf(50));
+// console.log(arr.length);
+// console.log(arr.pop());
+// console.log(arr.reverse());
+// console.log(arr.push());
+// console.log(arr.slice(1));
+// console.log(arr.includes(10,0));
+// console.log(arr.shift());
+// console.log(arr.unshift('a','b'));
+// console.log(arr.sort((a,b)=>b-a));
+// console.log(arr.toString());
+// console.log(arr.map(statement));
+// console.log(arr.find(statement));
+// console.log(arr.filter(statement));
+// console.log(arr.splice(2));
+// console.log(arr.copyWithin(3,5));
+// console.log(arr.fill(1,2,4));
+console.log(arr.entries());
+console.log(arr);
